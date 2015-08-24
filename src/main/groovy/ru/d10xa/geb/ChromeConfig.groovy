913f1d71b3c8461cb0e634e16621b22c4a5d1d2c
d10xa <d10xa@mail.ru>
2015-08-24 16:49:36 +0300

    some changes

--- src/main/groovy/ru/d10xa/geb/ChromeConfig.groovy
+++ src/main/groovy/ru/d10xa/geb/ChromeConfig.groovy
@@ -1,4 +1,21 @@
 package ru.d10xa.geb
 
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.api.Project
+
 class ChromeConfig {
+
+    static final String PROPERTY_DRIVER_CLASSNAME = 'org.openqa.selenium.chrome.ChromeDriver'
+    static final String PROPERTY_GEB_CLASSNAME = 'chrome'
+
+    Project project;
+
+    String getDriverPath() {
+        def chromedriverFilename = Os.isFamily(Os.FAMILY_WINDOWS) ? "chromedriver.exe" : "chromedriver"
+        def files = project.tasks.getByName('unzipChromeDriver').outputs.files
+        def file = new File(files.singleFile, chromedriverFilename)
+        def path = file.absolutePath
+        path
+    }
+
 }
